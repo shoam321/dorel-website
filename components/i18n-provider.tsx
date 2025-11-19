9fb12979db140eb6afe3959bfb8949738c581897
shoam321 <tjh852321@gmail.com>
2025-11-19 20:34:43 +0200

    fix: use I18nextProvider from react-i18next (build error)

--- components/i18n-provider.tsx
+++ components/i18n-provider.tsx
@@ -1,8 +1,8 @@
-'use client'
+"use client"
 
-import { ReactNode } from 'react'
-import I18nextProvider from 'i18next/react'
-import i18n from '@/lib/i18n'
+import { ReactNode } from "react"
+import { I18nextProvider } from "react-i18next"
+import i18n from "@/lib/i18n"
 
 export function I18nProvider({ children }: { children: ReactNode }) {
   return (
